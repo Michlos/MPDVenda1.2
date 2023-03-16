75b5caabdf757438c273d3a7c2552fcede827f95
Michlos <michlos@live.com>
2023-03-15 21:34:52 -0300

    Incrementando DBContext

--- MPDVenda.DataAccess/Configuration/ContextBase.cs
+++ MPDVenda.DataAccess/Configuration/ContextBase.cs
@@ -3,6 +3,15 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 
 using MPDVenda.Model.ApplicationUser;
+using MPDVenda.Model.Enums.Endereco;
+using MPDVenda.Model.Enums.Filial;
+using MPDVenda.Model.Enums.Financeiro;
+using MPDVenda.Model.Enums.ItemVenda;
+using MPDVenda.Model.Enums.ParceiroNegocio;
+using MPDVenda.Model.Estoque;
+using MPDVenda.Model.Filial;
+using MPDVenda.Model.Financeiro;
+using MPDVenda.Model.ParceiroNegocio;
 
 using System;
 using System.Collections.Generic;
@@ -17,6 +26,32 @@ namespace MPDVenda.DataAccess.Configuration
         public ContextBase(DbContextOptions options) : base(options)
         {
         }
+
+        public DbSet<Parceiro> Parceiro { get; set; }
+        public DbSet<Endereco> Endereco { get; set; }
+        public DbSet<Banco> Banco { get; set; }
+        public DbSet<Caixa> Caixa { get; set; }
+        public DbSet<MovimentoCaixa> MovimentoCaixa { get; set; }
+        public DbSet<Filial> Filial { get; set; }
+        public DbSet<ItemVenda> ItemVenda { get; set; }
+        public DbSet<TipoParceiro> TipoParceiro { get; set; }
+        public DbSet<TipoPessoa> TipoPessoa { get; set; }
+        public DbSet<TipoCompraItem> TipoCompraItem { get; set; }
+        public DbSet<TipoItem> TipoItem { get; set; }
+        public DbSet<TipoMovimentacaoEstoque> TipoMovimentacaoEstoque { get; set; }
+        public DbSet<PartidaDobrada> PartidaDobrada { get; set; }
+        public DbSet<RegimeTributario> RegimeTributario { get; set; }
+        public DbSet<Bairro> Bairro { get; set; }
+        public DbSet<Cidade> Cidade { get; set; }
+        public DbSet<TipoEndereco> TipoEndereco { get; set; }
+        public DbSet<TipoLogradouro> TipoLogradouro { get; set; }
+        public DbSet<UnidadeFederacao> unidadeFederacao { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+
+
+
+
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
